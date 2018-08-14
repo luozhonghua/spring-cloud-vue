e036393a42581c08a942ea12b53ec8c5f223da5f
luozhonghua <172257861@qq.com>
2018-08-14 17:57:06 +0800

    update

--- fore-end/cloud-vue/src/components/Account/changePwd.vue
+++ fore-end/cloud-vue/src/components/Account/changePwd.vue
@@ -1,17 +1,17 @@
 <template>
 	<el-dialog ref="dialog" custom-class="w-400 h-300" title="修改密码">
 		<div class="ovf-auto">
-			<el-form ref="form" :model="form" :rules="rules" label-width="80px">
-				<el-form-item label="旧密码" prop="old_pwd">
-					<el-input v-model.trim="form.old_pwd"></el-input>
+			<el-form ref="formpass" :model="formpass" :rules="rules" label-width="80px" >
+				<el-form-item label="旧密码" prop="oldpwd">
+					<el-input v-model.trim="formpass.oldpwd"></el-input>
 				</el-form-item>
-				<el-form-item label="新密码" prop="new_pwd">
-					<el-input v-model.trim="form.new_pwd"></el-input>
+				<el-form-item label="新密码" prop="newpwd">
+					<el-input v-model.trim="formpass.newpwd"></el-input>
 				</el-form-item>
+        <div class="p-t-20">
+			    <el-button type="primary" class="fl m-l-20"   @click="edit('formpass')" :loading="isLoading"  >提交</el-button>
+		   </div>
 			</el-form>
-		</div>
-		<div class="p-t-20">
-			<el-button type="primary" class="fl m-l-20" :disabled="disable" @click="submit()">提交</el-button>
 		</div>
 	</el-dialog>
 </template>
@@ -20,22 +20,23 @@
 </style>
 <script>
   import http from '../../assets/js/http'
+  import fomrMixin from '../../assets/js/form_com'
 
   export default {
     data() {
       return {
-        disable: false,
-        form: {
+        isLoading: false,
+        formpass: {
           auth_key: '',
-          old_pwd: '',
-          new_pwd: ''
+          oldpwd: '',
+          newpwd: ''
         },
         rules: {
-          old_pwd: [
+          oldpwd: [
             { required: true, message: '请输入旧密码', trigger: 'blur' },
             { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
           ],
-          new_pwd: [
+          newpwd: [
             { required: true, message: '请输入新密码', trigger: 'blur' },
             { min: 6, max: 12, message: '长度在 6 到 12 个字符', trigger: 'blur' }
           ]
@@ -49,21 +50,25 @@
       close() {
         this.$refs.dialog.close()
       },
-      submit() {
-        this.$refs.form.validate((pass) => {
+      edit(formName) {
+        this.$refs[formName].validate((pass) => {
           if (pass) {
-            this.disable = !this.disable
-            this.apiPost('admin/setInfo', this.form).then((res) => {
+            console.info(pass)
+            console.info(this.formpass.oldpwd)
+            console.info(this.formpass.newpwd)
+            this.isLoading = !this.isLoading
+            this.apiPost('admin/setInfo', this.formpass).then((res) => {
               this.handelResponse(res, (data) => {
                 _g.toastMsg('success', '修改成功')
                 Lockr.rm('authKey')
                 Lockr.rm('authList')
                 Lockr.rm('sessionId')
+                console.info(data.oldpwd)
                 setTimeout(() => {
                   router.replace('/')
                 }, 1500)
               }, () => {
-                this.disable = !this.disable
+                this.isLoading = !this.isLoading
               })
             })
           }
@@ -73,6 +78,6 @@
     created() {
       this.form.auth_key = Lockr.get('authKey')
     },
-    mixins: [http]
+    mixins: [http, fomrMixin]
   }
 </script>
